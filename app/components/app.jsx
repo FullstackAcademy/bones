ca202b16f8e8cbb13b063d077b13d06e0111fbe3
Steve Kuo <skuo314@gmail.com>
2017-01-12 16:00:49 -0500

    navbar made

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import Navbar from '../components/Navbar'
 
 export default function (props) {
   return (
     <div id="main" className="container-fluid">
-      <div className="col-xs-2">
-        {/* Include a container */}
+      <div className="col-xs-10">
+        <Navbar />
       </div>
       <div className="col-xs-10">
-        {
-          props.children && React.cloneElement(props.children, props)
-        }
       </div>
       {/* Include a container */}
     </div>
