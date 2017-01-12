3a2666cf6a75a12c4a188046e30fea186dd60429
Steve Kuo <skuo314@gmail.com>
2017-01-12 16:22:28 -0500

    random commit

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -3,13 +3,23 @@ import Navbar from '../components/Navbar'
 
 export default function (props) {
   return (
-    <div id="main" className="container-fluid">
-      <div className="col-xs-10">
-        <Navbar />
+    <div id="main">
+    <Navbar />
+      <div className="parallax-container">
+        <div className="parallax"><img src="images/parallax1.jpg" /></div>
       </div>
-      <div className="col-xs-10">
+      <div className="section white">
+        <div className="row container">
+          <h2 className="header">Parallax</h2>
+          <p className="grey-text text-darken-3 lighten-3">Insert pic or other stuff here</p>
+        </div>
       </div>
+      <div className="parallax-container">
+        <div className="parallax"><img src="images/parallax2.jpg" /></div>
+      </div>
+
       {/* Include a container */}
     </div>
   );
 }
+
