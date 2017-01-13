83aef922e3301f1e8b0bcedaf2e85cdcbddc8ec7
Ben Zurkow <benjamin.zurkow@gmail.com>
2017-01-13 13:20:14 -0500

    product r&t work

--- server/products.js
+++ server/products.js
@@ -14,24 +14,35 @@ module.exports = require('express').Router()
 		})
 		.catch(next)
 	})
-	.get('/:id/reviews', (req, res, next) => {
-
-	})
-	.get('/:id/reviews/:reviewid', (req, res, next) => {
-
+	.post('/', (req, res, next) => {
+		console.log("adfadsfasdf")
+		Product.create(req.body)
+		.then(product => res.status(201).json(product))
+		.catch(next)
 	})
 	.get('/', (req, res, next) => {
-		
+		Product.findAll()
+		.then(products => res.json(products))
+		.catch(next)	
 	})
 	.get('/:id', (req, res, next) => {
-
-	})
-	.post('/:id', (req, res, next) => {
-
+		res.json(req.requestedProduct)
+		.catch(next)
 	})
 	.put('/:id', (req, res, next) => {
-
+		req.requestedProduct.update(req.body)
+		.then(updatedProduct => {
+			res.send(updatedProduct)
+		});
 	})
 	.delete(':/id', (req, res, next) => {
-
+		req.requestedUser.destroy()
+		.then(()=>res.status(204).end())
+		.catch(next);
 	})
+	// .get('/:id/reviews', (req, res, next) => {
+
+	// })
+	// .get('/:id/reviews/:reviewid', (req, res, next) => {
+
+	// })
--- server/products.test.js
+++ server/products.test.js
@@ -4,34 +4,87 @@ const db = require('APP/db')
 const Product = require('APP/db/models/product')
 const app = require('./start')
 
-describe('/api/products', () => {
+describe.only('/api/products', () => {
 	before('wait for the db', () => db.didSync)
-  	after('synchronize and clear database', () => db.sync({force: true}));
+  //after('synchronize and clear database', () => db.sync({force: true}));
 
-  	it('POSTs one product', () => {
+  let product1, product2, product3
 
-  	});
+  before('Seed Products', () => {
+    const products = [
+      {
+        title: "Ben's Best Blue",
+        brand: "Ben's",
+        category: "paint",
+        description: "Ben's Best",
+        unitPrice: 21.21,
+        inventoryQuantity: 21,
+      },
+      {
+        title: "Bubba's Special Stick",
+        brand: "A Moderately Sized Tree",
+        category: "painting tools",
+        description: "From the one and only A Moderately Sized Tree. Tree was not harmed in the making of this stick.",
+        unitPrice: 5000.00,
+        inventoryQuantity: 1,
+      },
+      {
+        title: "Coarse Sand",
+        brand: "The Quarry",
+        category: "prep",
+        description: "Coarse sand - perfect for removal of anything from anything. And removal of that thing",
+        unitPrice: 2.00,
+        inventoryQuantity: 500,
+      }
+    ]
 
-  	it('GETs all products', () => {
+    return Product.bulkCreate(products, {returning: true})
+      .then(createdProduct => {
+       product1  = createdProduct[0].id;
+       product2  = createdProduct[1].id;
+       product3  = createdProduct[2].id;
 
-  	});
-
-  	it('GETs one product', () => {
-
-  	});
+    })
+  })
 
 
-  	it('PUTs one product', () => {
+	it('POSTs one product', () => {
+    return request(app)
+      .post('/api/products')
+      .send({
+        title: 'Davids Cobalt Blast',
+        brand: 'Bubba Gump David',
+        category: 'paint',
+        description: 'A blast from the past of extroardinary color',
+        unitPrice: 33.77,
+        inventoryQuantity: 37
+      })
+      .then(res => {
+        console.log(res.body, res.status)
+      })
+  });
 
-  	});
+	xit('GETs all products', () => {
+    request(app)
 
-  	it('DELETEs one products', () => {
+	});
 
-  	});
-  	it('GETS all reviews for a product', () => {
+	xit('GETs one product', () => {
 
-  	});
-  	it('GETS one review for a product', () => {
+	});
 
-  	})
+	xit('PUTs one product', () => {
+
+	});
+
+	xit('DELETEs one products', () => {
+
+	});
+	// it('GETS all reviews for a product', () => { 
+
+	// });
+	// it('GETS one review for a product', () => {
+		
+	// })
+
 })
