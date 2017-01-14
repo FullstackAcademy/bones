a7615f2b825a91bbfb95ba4231f9984c6d2bff5d
Maz Black <mazharrr@live.com>
2017-01-14 14:42:50 -0500

    Update Cart.jsx

--- app/action-creators/Cart.jsx
+++ app/action-creators/Cart.jsx
@@ -12,16 +12,16 @@ const receiveProducts = function(products) {
 
 export const addToCart = function(productId) {
 	return function(dispatch, getState) {
-    console.log('testing to see if i have active order')
+   // console.log('testing to see if i have active order')
     if(!getState().order){
-      console.log('I do not have an active order')
+    //  console.log('I do not have an active order')
       axios.post('api/orders', {})
       .then(newOrder =>{
-        console.log(newOrder)
+    //    console.log(newOrder)
       })
     }
     else {
-      console.log('I have an active order')
+  //    console.log('I have an active order')
     }
 
 
