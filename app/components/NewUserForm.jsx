8fef80870afb5cf27696ed58f3ab7ac0ff0fbbf2
David Rhodes <rhodesdav@gmail.com>
2017-01-15 21:55:07 -0500

    fixes #77

--- app/components/NewUserForm.jsx
+++ app/components/NewUserForm.jsx
@@ -15,13 +15,11 @@ export default class NewUserForm extends Component {
 			email: '',
 			password: ''
 		};
-
 		this.handleUserNameChange = this.handleUserNameChange.bind(this);
 		this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
 		this.handleLastNameChange = this.handleLastNameChange.bind(this);
 		this.handleEmailChange = this.handleEmailChange.bind(this);
 		this.handlePasswordChange = this.handlePasswordChange.bind(this);
-
 		this.handleSubmit = this.handleSubmit.bind(this);
 }
 
@@ -54,22 +52,6 @@ handleSubmit(event) {
 	.then(response => store.dispatch(login(response.data.email, response.data.password)))
 	.then( () => hashHistory.push('/user'))
 // here we need to handle button change to edit once logged in
-	.catch(err => console.error(err.stack));
-}
-
-handleEdit(event) {
-	event.preventDefault();
-	// On submit of the form, send a POST request with the data to the server.
-	 return axios.post('/api/users', {
-		userName: this.state.userName,
-		firstName: this.state.firstName,
-		lastName: this.state.lastName,
-		email: this.state.email,
-		password: this.state.password
-	})
-	.then(response => store.dispatch(login(response.data.email, response.data.password)))
-	.then( () => hashHistory.push('/user'))
-// here we need to handle button change to edit
 	.catch(err => console.error(err.stack));
 }
 
